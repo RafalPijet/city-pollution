18f59ca4cc9e4d87ce40312527085a9a48c545a1
Rafal Pijet <stronglopez@wp.pl>
2020-01-08 20:30:38 +0100

    Changing components InfoItem, InfoBox from class to functional unisng hooks

--- src/components/common/InfoItem/InfoItem.js
+++ src/components/common/InfoItem/InfoItem.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import HtmlBox from '../../common/HtmlBox/HtmlBox';
 import {Collapse, CardBody, Card} from 'reactstrap';
 import './InfoItem.scss';
 
+const InfoItem = props => {
+    const {i, selectedItem, collapse, city} = props;
+    const [isOpen, setIsOpen] = useState(false);
+    const [number, setNumber] = useState(0);
+
+    useEffect(() => {
+        setNumber(i);
+        setIsOpen(selectedItem === number);
+    }, [i, selectedItem, number]);
+
+    const openHandling = () => {
+        setIsOpen(true);
+        collapse(number);
+    };
+
+    return (
+        <div>
+            <div className='info-header-row' onClick={openHandling}>
+                <p className='info-header'>{city.name}</p>
+            </div>
+            <Collapse isOpen={isOpen}>
+                <Card>
+                    <CardBody className='info-item-body'>
+                        <HtmlBox>{city.description.length !== 0 ? city.description :
+                            '<p>Sorry, but description is not available!</p>'}</HtmlBox>
+                    </CardBody>
+                </Card>
+            </Collapse>
+        </div>
+    )
+};
+/*
 class InfoItem extends React.Component {
     state = {
         isOpen: false,
@@ -47,11 +79,12 @@ class InfoItem extends React.Component {
         )
     }
 }
-
+*/
 InfoItem.propTypes = {
     collapse: PropTypes.func.isRequired,
     i: PropTypes.number.isRequired,
-    selectedItem: PropTypes.number.isRequired
+    selectedItem: PropTypes.number.isRequired,
+    city: PropTypes.object.isRequired
 };
 
 export default InfoItem;
--- src/components/features/InfoBox/InfoBox.js
+++ src/components/features/InfoBox/InfoBox.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import InfoItem from '../../common/InfoItem/InfoItem';
 import {sortByValue} from "../../../utilities/functions";
@@ -6,6 +6,76 @@ import Spinner from '../../common/Spinner/Spinner';
 import {Animated} from "react-animated-css";
 import './InfoBox.scss';
 
+const InfoBox = props => {
+    const {pollution, request, cities, loadCities} = props;
+    const [typePollution, setTypePollution] = useState('');
+    const [selectedItem, setSelectedItem] = useState(0);
+    const [isInfo, setIsInfo] = useState(false);
+
+    useEffect(() => {
+        if (pollution.type !== typePollution) {
+            setTypePollution(pollution.type);
+            setSelectedItem(0);
+        }
+        setIsInfo(request.info)
+    }, [pollution, request, typePollution]);
+
+    useEffect(() => {
+        if (pollution.type !== typePollution) {
+            setSelectedPollution(pollution);
+        }
+    });
+
+    const collapseHandling = index => {
+        setSelectedItem(index);
+    };
+
+    const setSelectedPollution = pollution => {
+        switch (pollution.type) {
+            case 'Pm25':
+                loadCities(pollution.pm25);
+                break;
+            case 'Pm10':
+                loadCities(pollution.pm10);
+                break;
+            case 'So2':
+                loadCities(pollution.so2);
+                break;
+            case 'No2':
+                loadCities(pollution.no2);
+                break;
+            default:
+        }
+    };
+
+    let citiesAfterSort = cities.sort(sortByValue);
+    if (citiesAfterSort.length < 10) {
+        return (
+            <div className='info-box-spinner'>
+                <Spinner/>
+            </div>
+        )
+    } else if (isInfo && citiesAfterSort.length === 10) {
+        return (
+            <div className='info-box-main'>
+                <Animated animationIn='flipInY'
+                          isVisible={isInfo && citiesAfterSort.length === 10}>
+                    {citiesAfterSort.map((city, i) => {
+                        return <InfoItem selectedItem={selectedItem} i={i}
+                                         collapse={collapseHandling} key={i} city={city}/>
+                    })}
+                </Animated>
+            </div>
+        )
+    } else {
+        return (
+            <div className='info-box-spinner'>
+                <p className='text-center'>Cities description</p>
+            </div>
+        )
+    }
+};
+/*
 class InfoBox extends React.Component {
     state = {
         typePollution: '',
@@ -87,7 +157,7 @@ class InfoBox extends React.Component {
         }
     }
 }
-
+*/
 InfoBox.propTypes = {
     loadCities: PropTypes.func.isRequired,
     request: PropTypes.object.isRequired,
